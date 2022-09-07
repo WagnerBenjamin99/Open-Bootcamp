aa489cda21f85f9feafb37d6637fc5fe94188bd1
Benjamin <benjaminwagner19@outlook.com>
2022-09-07 20:33:24 -0300

    aa

--- Curso de Python/Clases y Objetos/Prueba.py
+++ Curso de Python/Clases y Objetos/Prueba.py
@@ -1,6 +1,10 @@
+from unicodedata import name
+
+
 class robot:
     _encendido = False # _ indica que no debe ser modificada fuera de la clase
-
+    def __init__(self, nombre):
+        self.name = nombre
     def enciende(self): #prennde robot
         self._encendido=True #con el self accedo a la variable de la clase
     
@@ -10,7 +14,13 @@ class robot:
     def estado(self): #devuelve estado del robot(apagado o encendido) 
         return self._encendido
 
+class perro(robot): #hereda caracteristicas y metodos de la clase robot
+    pass
 
-robot1=robot()
 
-print(robot1.estado)
+
+robot1=robot('Benjamin') #inicializo nombre(constructor)
+print(robot1.name)
+
+robot2=robot('Juan')
+print(robot2.name)
