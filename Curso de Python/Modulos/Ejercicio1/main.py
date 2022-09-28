e1306f51f4ea4df2cfb77bff0b8913af560cbe4d
Benjamin <benjaminwagner19@outlook.com>
2022-09-28 15:48:32 -0300

    Modulos - Ejercicio1

---
+++ Curso de Python/Modulos/Ejercicio1/main.py
@@ -0,0 +1,6 @@
+import operaciones as o
+
+print('Suma:', o.sumador(10, 5))
+print('Resta:', o.restador(10, 5))
+print('Multiplicacion:', o.multiplicador(10, 5))
+print('Division:', o.divisor(10, 5))
---
+++ Curso de Python/Modulos/Ejercicio1/operaciones.py
@@ -0,0 +1,8 @@
+def sumador(x, y):
+    return x + y
+def restador(x,y):
+    return x - y
+def multiplicador(x, y):
+    return x * y
+def divisor(x, y):
+    return x / y
